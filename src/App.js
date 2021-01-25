c5c9fa2cb278ac4c84e17e99fa26a2b786687885
caozhengqian@deppon.com <caozhengqian@deppon.com>
2021-01-25 14:58:34 +0800

    接入antd-mobile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd-mobile';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       <Button>ddddddddddddddddd</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
